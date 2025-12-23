0e265d5450e7c5a899929c23ce52006795a289b9
James Andrews <james@societyofgenius.com>
2025-12-23 11:00:03 +0000

    working algo for part2. Refactored with utils

---
+++ aocutilities/aocutilities.go
@@ -0,0 +1,58 @@
+package aocutilites
+
+// Stack
+
+type Stack[T any] struct {
+	items []T
+}
+
+func (s *Stack[T]) Push(item T) {
+	s.items = append(s.items, item)
+}
+
+func (s *Stack[T]) Pop() (T, bool) {
+	if len(s.items) == 0 {
+		var zero T
+		return zero, false
+	}
+	item := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	return item, true
+}
+
+func (s *Stack[T]) Peek() (T, bool) {
+	if len(s.items) == 0 {
+		var zero T
+		return zero, false
+	}
+	return s.items[len(s.items)-1], true
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.items) == 0
+}
+
+func (s *Stack[T]) Size() int {
+	return len(s.items)
+}
+
+// String and array manipulation
+
+func SplitWords(s string) []string {
+	var words []string
+	currentWord := ""
+	for _, char := range s {
+		if char == ' ' || char == '\t' {
+			if currentWord != "" {
+				words = append(words, currentWord)
+				currentWord = ""
+			}
+		} else {
+			currentWord += string(char)
+		}
+	}
+	if currentWord != "" {
+		words = append(words, currentWord)
+	}
+	return words
+}
